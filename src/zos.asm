0706e6716fa9c79c2dd88db38585dc518b101b01
David Higgins <zoul0813@me.com>
2025-01-19 15:59:35 -0500

    * switch to local labels * added KB_MODE macro * set raw kb mode by default * use cooked mode in OSLINE * added "code folding" comment markers * refactor OSRDCH to use OSKEY (similar to cpm.asm)

--- src/zos.asm
+++ src/zos.asm
@@ -36,6 +36,15 @@
         EXTERN	USER
 
 
+        MACRO KB_MODE MODE
+                ; raw mode
+                ld h, DEV_STDIN
+                ld c, KB_CMD_SET_MODE
+                ld de, MODE
+                IOCTL()
+        ENDM
+
+
 ;OSINIT - Initialise RAM mapping etc.
 ;If BASIC is entered by BBCBASIC FILENAME then file
 ;FILENAME.BBC is automatically CHAINed.
@@ -46,9 +55,11 @@
 ;
 OSINIT:
         di       ; disable interrupts
+        KB_MODE(KB_READ_NON_BLOCK | KB_MODE_RAW)        ; default to raw keyboard for INKEYs
+
         ld a, b
         or c
-        jr z, SKIPLOAD
+        jr z, @skipload
         ; load file
 
         ld hl, ACCS ; copy filename to string buffer
@@ -56,10 +67,11 @@ OSINIT:
         ldir
         ; Reset Z flag
         or 1
-SKIPLOAD:
+@skipload:
         ld de, 0xF800
         ld hl, USER ; start of user memory
         ret
+;
 
 ;PTEXT - Print text
 ;   Inputs: HL = address of text
@@ -77,56 +89,17 @@ CPTEXT:
         pop bc
         pop de
         ret
-
+;
 
 ;OSRDCH - Read from the current input stream (keyboard).
 ;  Outputs: A = character
 ; Destroys: A,F
 ;
-KEYGET:
 OSRDCH:
-        push bc
-        push de
-        push hl
-
-        ; raw mode
-        ld h, DEV_STDIN
-        ld c, KB_CMD_SET_MODE
-        ld de, KB_READ_NON_BLOCK | KB_MODE_RAW
-        IOCTL()
-
-        ei
-        ; loop until a key is pressed
-WAITFORKEY:
-        S_READ3(DEV_STDIN, BUFFER, 3)
-        or a
-        jr nz, KEYERROR
-        ; A is zero for sure, OR it with C to check if C is 0
-        or c
-        jr z, WAITFORKEY ; if no key
-        ld a, (de) ; get the code from BUFFER[0]
-        cp KB_RELEASED
-        jr z, WAITFORKEY
-        jp KEYREAD
-KEYERROR:
-        ; an error has occurred, handle it
-        ; DE still contains the buffer address
-        ld (de), a
-KEYREAD:
-        di
-
-        ; go back to cooked mode
-        ld h, DEV_STDIN
-        ld c, KB_CMD_SET_MODE
-        ld de, KB_READ_BLOCK | KB_MODE_COOKED
-        IOCTL()
-
-        pop hl
-        pop de
-        pop bc
-
-        ld a, (BUFFER) ; get the code from BUFFER[0]
-        ret
+        call OSKEY      ; check for a key
+        ret c           ; return if carry set
+        jr OSRDCH       ; loop until key is press
+;
 
 ;OSWRCH - Write a character to console output.
 ;   Inputs: A = character.
@@ -144,6 +117,7 @@ OSWRCH:
         pop bc
         pop af
         ret
+;
 
 ;OSLINE - Read/edit a complete line, terminated by CR.
 ;   Inputs: HL addresses destination buffer.
@@ -153,14 +127,25 @@ OSWRCH:
 ; Destroys: A,B,C,D,E,H,L,F
 ;
 OSLINE:
+        push hl ; save buffer address
+        KB_MODE(KB_READ_BLOCK | KB_MODE_COOKED) ; switch to cooked to read a "line"
+        pop hl  ; pop buffer address
+
         ex de, hl
         ld bc, 254
         ei
         S_READ1(DEV_STDIN)
         di
         xor a
-        ret
 
+        push af ; save S_READ1 error
+        push hl ; save buffer address
+        KB_MODE(KB_READ_NON_BLOCK | KB_MODE_RAW); switch back to raw
+        pop hl  ; pop buffer address
+        pop af  ; pop the S_READ1 error
+
+        ret
+;
 
 ;OSOPEN - Open a file for reading or writing.
 ;   Inputs: HL addresses filename (term 0)
@@ -175,17 +160,17 @@ OSOPEN:
         ld c, l
         ; Set the flags
         ld h, O_RDONLY
-        jr c, OSOPEN_READ
+        jr c, @read
         ld h, O_WRONLY | O_CREAT | O_APPEND
-OSOPEN_READ:
+@read:
         OPEN()
         ; Check for error, if positive, return
         or a
         ret p
-OSOPEN_ERROR:
+@error:
         xor a
         ret
-
+;
 
 ;OSSTAT - Read file status.
 ;   Inputs: E = file channel
@@ -196,6 +181,7 @@ OSOPEN_ERROR:
 ;
 OSSTAT:
         ret
+;
 
 
 ;OSBGET - Read a byte from a random disk file.
@@ -215,17 +201,18 @@ OSBGET:
         ; be done but it's the simplest solution for now
         ld a, b
         or c
-        jr z, OSBGET_LAST
+        jr z, @last
         ; No way to return an error, make the assumption it's a success
         ld a, (de)
         pop hl
         pop de
         ret
-OSBGET_LAST:
+@last:
         scf
         pop hl
         pop de
         ret
+;
 
 ;OSBPUT - Write a byte to a random disk file.
 ;   Inputs: E = file channel
@@ -244,6 +231,7 @@ OSBPUT:
         pop hl
         pop de
         ret
+;
 
 ;OSLOAD - Load an area of memory from a file.
 ;   Inputs: HL addresses filename (NULL-term)
@@ -275,6 +263,7 @@ OSLOAD:
         ret
 
 OSLOAD_SIZE: DEFW 0x2000
+;
 
 ;OSSAVE - Save an area of memory to a file.
 ;   Inputs: HL addresses filename (term CR)
@@ -300,11 +289,14 @@ OSSAVE:
         WRITE()
         CLOSE()
         ret
+;
+
+;
 SHOW_ERROR_NEG:
         neg
 SHOW_ERROR:
         ret
-
+;
 
 ;OSSHUT - Close disk file(s).
 ;   Inputs: E = file channel
@@ -318,7 +310,7 @@ OSSHUT:
     ld h, a
     CLOSE()
     ret
-
+;
 
 ;
 ;GETEXT - Find file size.
@@ -332,16 +324,17 @@ GETEXT:
         DSTAT()
         ; Check the error, return 0 in that case
         or a
-        jr nz, GETEXT_ERROR
+        jr nz, @error
         ; Dereference the size from the structure
         ld hl, (BUFFER)
         ld de, (BUFFER + 2)
         ret
-GETEXT_ERROR:
+@error:
         ld de, 0
         ld h, d
         ld l, e
         ret
+;
 
 ;
 ;GETPTR - Return file pointer.
@@ -357,17 +350,18 @@ GETPTR:
         ld e, c
         ld a, SEEK_CUR
         SEEK()
-        jr nz, GETPTR_ERROR
+        jr nz, @error
         ld h, b
         ld l, c
         ex de, hl
         ret
-GETPTR_ERROR:
+@error:
         ; Return 0 in terms of error
         ld de, 0
         ld h, d
         ld l, e
         ret
+;
 
 ;
 ;PUTPTR - Update file pointer.
@@ -385,16 +379,18 @@ PUTPTR:
         ld a, SEEK_SET
         SEEK()
         ret
+;
 
-
+;
 PROMPT:
         ld a, '>'
         call OSWRCH
         ld a, ' '
         jp OSWRCH
+;
 
 ;
-;BYE - Stop interrupts and return to CP/M.
+;BYE - Stop interrupts and return to Shell.
 ;
 BYE:
         ld h, 0 ; Return value
@@ -414,6 +410,7 @@ OPTVAL: DEFS 1
 LTRAP:
         xor a
         ret
+;
 
 ;TRAP - Test ESCAPE flag and abort if set;
 ;       every 20th call, test for keypress.
@@ -421,13 +418,16 @@ LTRAP:
 ;
 TRAP:
         ret
+;
 
 ;
 ;OSCLI - Process an "operating system" command
 ;
 OSCLI:
+        ; TODO: implement more than "BYE"
         jr BYE
         ret
+;
 
 ;
 ;OSKEY - Read key with time-limit, test for ESCape.
@@ -438,11 +438,43 @@ OSCLI:
 ; Destroys: A,H,L,F
 ;
 OSKEY:
-        xor a
-        ret
+        push bc
+        push de
+        push hl
 
+        ei
+@getkey:
+        S_READ3(DEV_STDIN, BUFFER, 3)
+        or a    ; did an error occur?
+        jr nz, @error
+        or c    ; did we read any bytes?
+        jr z, @error ; if no key
+
+        ld a, (de) ; get the code from BUFFER[0]
+        cp KB_RELEASED  ; ignore key releases
+        jr z, @error
+
+        scf
+        jp @read
+@error:
+        ; an error has occurred, handle it
+        xor a   ; clear carry flag
+@read:
+        di
+
+        pop hl
+        pop de
+        pop bc
+
+        ld a, (BUFFER) ; get the code from BUFFER[0]
+        ret
+;
+
+;
 OSCALL:
         ret
+;
 
+;
     ; Make sure it's big enough for storing the stat
 BUFFER: DEFS ZOS_STAT_SIZE
